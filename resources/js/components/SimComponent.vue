d909ebc4e831b6716501e203a672f8c6893a3d0d
victorsv79 <victor@ares-ufa.ru>
2023-07-16 12:56:23 +0500

    add Laravel to project

--- resources/js/components/SimComponent.vue
+++ resources/js/components/SimComponent.vue
@@ -1,10 +1,8 @@
 <template>
     <div>
         <h2 class="text-center">Список SIM-карт</h2>
-        <div class="row">
-            <div class="col-sm-9">
-            </div>
-            <div class="col-sm-3">
+        <div class="row mb-2">
+            <div class="offset-sm-7 col-sm-5">
                 <select required class='form-control me-2'  v-model='tarif_id' @change='getSims()'>
                     <option value='0' selected>- Фильтр по тарифному плану -</option>
                     <option v-for='tarif in tarifs' :value='tarif.id'>{{ tarif.name }}</option>
@@ -12,35 +10,31 @@
             </div>
         </div>
 
-        <table class="table table-hover">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>IMEI</th>
-                <th>Телефон</th>
-                <th>ФИО</th>
-                <th>Дата регистрации</th>
-                <th>Тарифный план</th>
-                <th>Действия</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="sim in sims" :key="sim.id">
-                <td>{{ sim.id }}</td>
-                <td>{{ sim.imei }}</td>
-                <td>{{ sim.phone }}</td>
-                <td>{{ sim.fio }}</td>
-                <td>{{ sim.date_reg }}</td>
-                <td>{{ tarifName(sim.tarif_id) }}</td>
-                <td>
-                    <div role="group">
-                        <router-link :to="{name: 'edit', params: { id: sim.id }}" class="btn btn-success m-1">Редактировать</router-link>
-                        <button class="btn btn-danger m-1" @click="deleteSim(sim.id)">Удалить</button>
-                    </div>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+        <div class="row mb-3 fw-bold">
+            <div class="col-lg-1 d-none d-md-block">ID</div>
+            <div class="col-md-2 col-12">ФИО</div>
+            <div class="col-md-2 col-6">IMEI</div>
+            <div class="col-md-2 col-6">Телефон</div>
+            <div class="col-md-2 col-6">Дата регистрации</div>
+            <div class="col-md-2 col-6">Тарифный план</div>
+            <div class="col-md-1 col-12">Действия</div>
+        </div>
+
+        <div  class="row mb-3" v-for="sim in sims" :key="sim.id">
+            <hr>
+            <div class="col-lg-1 d-none d-md-block fw-bold">{{ sim.id }}</div>
+            <div class="col-md-2 col-12 fw-bold">{{ sim.fio }}</div>
+            <div class="col-md-2 col-6">{{ sim.imei }}</div>
+            <div class="col-md-2 col-6">{{ sim.phone }}</div>
+            <div class="col-md-2 col-6">{{ sim.date_reg }}</div>
+            <div class="col-md-2 col-6">{{ tarifName(sim.tarif_id) }}</div>
+            <div class="col-md-1 col-12">
+                <div role="group">
+                    <router-link :to="{name: 'edit', params: { id: sim.id }}" class="btn btn-success m-1">Редактировать</router-link>
+                    <button class="btn btn-danger m-1" @click="deleteSim(sim.id)">Удалить</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
